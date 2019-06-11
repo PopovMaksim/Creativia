9c8e211a5d896f68b22263893033f8c83f4b68c6
Maksim <50889063+PopovMaksim@users.noreply.github.com>
2019-06-11 17:23:54 +0300

    анимация кнопки меню

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function openmenu() {
 	var nav = document.getElementById('nav');
 	nav.classList.toggle("menu-open");
-
+	
+	var menu = document.getElementById('menu');
+	menu.classList.toggle("menu__open");
 }
