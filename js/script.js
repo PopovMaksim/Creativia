0a1aaa1ccac08e6360364b318dc7735125ca9d41
Maksim <50889063+PopovMaksim@users.noreply.github.com>
2019-06-14 23:09:14 +0300

    кнопки вперед назад для слайдера

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ function openmenu() {
 	var menu = document.getElementById('menu');
 	menu.classList.toggle("menu__open");
 }
+
+
+var back = document.getElementById('slide-back');
+var next = document.getElementById('slide-next');
+var background = document.getElementById('header');
+var i = 1;
+
+back.onclick = function back_slide() {
+	i--;
+	if (i<=0) {
+		i = 3;
+	}
+	background.style.backgroundImage = "url('images/slider-bg/img" + i + ".png')";
+}
+
+next.onclick = function next_slide() {
+	i++;
+	if (i >= 4) {
+		i = 1;
+	}
+	background.style.backgroundImage = "url('images/slider-bg/img" + i + ".png')";
+}
